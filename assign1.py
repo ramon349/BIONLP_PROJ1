ebf922635c4ff0cd3bab101a1913b2079699610f
Ramon Correa <ramoncorrea@Ramons-MacBook-Pro.local>
2020-09-19 20:50:15 -0400

    Did to do list items 1. Add a list that stores the original text match. (used for symptom expression) 2. Convert the match onto the standard symptom variable. This will need a dict that matches CUIS to symptoms 3. Once 1 and 2 are done we need to update the pandas series. Returning a filled out dataframe. If modification in place does not work. We return a list of series and concatenate them. (ensure insertion order)

--- assign1.py
+++ assign1.py
@@ -46,13 +46,17 @@ def gen_negation_range(neg_match):
             out.append(groups)
     return out 
 
-def add_note(symp_dict,symp_matches,text,negation_ranges): 
-    tags = list() 
+def add_note(symp_dict,symp_matches,text,negation_ranges,code_2_gen): 
+    terms = list() 
     negs = list()
-    for e in symp_matches:   
+    cuis = list() 
+    gen_symps = list()
+    for e in symp_matches: 
         term = e.groups(1)[0] 
         moded = False  
-        tags.append(term)
+        terms.append(term)
+        cuis.append(symp_dict[term])
+        gen_symps.append(code_2_gen[symp_dict[term]])
         for k in negation_ranges: 
             if term in k: 
                 negs.append("1")
@@ -60,25 +64,31 @@ def add_note(symp_dict,symp_matches,text,negation_ranges):
                 break 
         if not moded: 
             negs.append("0")
-    return ("$$$".join(tags),"$$$".join(negs))
-def annotate_individual(sample:pd.Series,symp_dict,negations):
+    return ("$$$".join(terms),"$$$".join(gen_symps),"$$$".join(cuis), "$$$".join(negs))
+def annotate_individual(sample:pd.Series,symp_dict,negations,code_2_gen):
     ID = sample['ID']
     txt = sample['TEXT']
     o1 = search_symptoms(symp_dict,txt)
     o2 =search_negation(negations,txt)
     neg_range = gen_negation_range(o2)
-    (cuis,negationFlag)= add_note(symp_dict,o1,txt,neg_range)
-    print("CUIS")
-    print(cuis)
-    print('negations')
-    print(negationFlag)
+    (matched,generic, cuis, negations)= add_note(symp_dict,o1,txt,neg_range,code_2_gen)
+    sample['Symptom Expressions'] = matched 
+    sample['Standard Symptom'] = generic 
+    sample['Symptom CUIS'] = cuis 
+    sample['Negation Flag'] = negations 
+    return sample
+
+
 def build_symps():
-    lexicon :pd.DataFrame= pd.read_csv('COVID-Twitter-Symptom-Lexicon.txt',sep='\t',names=['Gen','code','desc'])
+    lexicon :pd.DataFrame= pd.read_csv('COVID-Twitter-Symptom-Lexicon.txt',sep='\t',names=['gen','code','desc'])
     symp_dict = {} 
+    code_2_gen= {}
     for _,e in lexicon.iterrows():
         (term_id,words) = (e['code'],e['desc'])
+        gen = e['gen']
         symp_dict[words] = term_id
-    return symp_dict
+        code_2_gen[term_id] = gen
+    return (symp_dict,code_2_gen)
 def build_negations(): 
     rem_regex = r'(\.\s|\s)?(\w*\b)?(\.\s|\s)?(\w*\b)?(\.\s|\s)?(\w*\b)?' #this is the regex that matches the foward parts 
     neg_text = [ r"(\b{}\b){}".format(e,rem_regex) for e in open('./neg_trigs.txt').read().split('\n') ]
@@ -87,8 +97,11 @@ def main():
     pass 
 if __name__=="__main__":
     negs = build_negations()
-    symps = build_symps()
+    symps,code_2_gen = build_symps()
     input_file = sys.argv[1]
     reddit = pd.read_excel(input_file)
+    processed = list()
     for index,row in reddit.iterrows() : 
-        annotate_individual(row,symps,negs)
+        processed.append (annotate_individual(row,symps,negs,code_2_gen) )
+    final = pd.concat(processed,axis=1).T
+    final.to_excel('pokemon_test.xlsx')
